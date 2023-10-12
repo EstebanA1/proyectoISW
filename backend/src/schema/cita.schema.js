22c4d2c8a9f3ab2e713fbb4f8748823efd8b2564
EstebanA1 <esteban.rivas2001@alumnos.ubiobio.cl>
2023-10-12 13:13:22 -0300

    Se creo el schema de las citas

---
+++ backend/src/schema/cita.schema.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const Joi = require("joi");
+
+/**
+ * Esquema de validación para el cuerpo de la solicitud de la cita.
+ * @constant {Object}
+ */
+const citaBodySchema = Joi.object({
+    name: Joi.string().required().messages({
+        "string.empty": "El nombre del solicitante de la cita no puede estar vacío.",
+        "any.required": "El nombre del solicitante de la cita es obligatorio.",
+        "string.base": "El nombre del solicitante de la cita debe ser de tipo string.",
+    }),
+    typeOfRequest: Joi.string().required().messages({
+        "string.empty": "El tipo de solicitud de la cita no puede estar vacío.",
+        "any.required": "El tipo de solicitud de la cita es obligatorio.",
+        "string.base": "El tipo de solicitud de la cita debe ser de tipo string.",
+    }),
+    date: Joi.string().required().messages({
+        "string.empty": "La fecha de la cita no puede estar vacío.",
+        "any.required": "La fecha de la cita es obligatorio.",
+        "string.base": "La fecha de la cita debe ser de tipo string.",
+    }), // PROBABLE A CAMBIAR
+}).messages({
+    "object.unknown": "No se permiten propiedades adicionales.",
+});
+
+/**
+ * Esquema de validación para el id de usuario.
+ * @constant {Object}
+ */
+const citaIdSchema = Joi.object({
+    id: Joi.string()
+        .required()
+        .pattern(/^(?:[0-9a-fA-F]{24}|[0-9a-fA-F]{12})$/)
+        .messages({
+            "string.empty": "El id no puede estar vacío.",
+            "any.required": "El id es obligatorio.",
+            "string.base": "El id debe ser de tipo string.",
+            "string.pattern.base": "El id proporcionado no es un ObjectId válido.",
+        }),
+});
+
+module.exports = { citaBodySchema, citaIdSchema };
