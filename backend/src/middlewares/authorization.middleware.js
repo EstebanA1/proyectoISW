8dbab912e4f942102778604b52c6bd3b699fd3b8
EstebanA1 <esteban.rivas2001@alumnos.ubiobio.cl>
2023-10-17 19:55:44 -0300

    Se agregaron las validaciones de los distintos roles

--- backend/src/middlewares/authorization.middleware.js
+++ backend/src/middlewares/authorization.middleware.js
@@ -13,25 +13,92 @@ const { handleError } = require("../utils/errorHandler.js");
  */
 async function isAdmin(req, res, next) {
   try {
+    console.log("Pase por la autorización");
     const user = await User.findOne({ email: req.email });
+    console.log("El email es ", req.email);
+    console.log("Los roles son ", user.roles);
     const roles = await Role.find({ _id: { $in: user.roles } });
-    for (let i = 0; i < roles.length; i++) {
-      if (roles[i].name === "admin") {
+
+    for (const role of roles) {
+      if (role.name === "administrador") {
         next();
         return;
       }
     }
+
     return respondError(
       req,
       res,
       401,
-      "Se requiere un rol de administrador para realizar esta acción",
+      "Se requiere un rol de administrador para realizar esta acción"
     );
   } catch (error) {
     handleError(error, "authorization.middleware -> isAdmin");
   }
 }
 
+/**
+ * Comprueba si el usuario es solicitante
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ * @param {Function} next - Función para continuar con la siguiente función
+ */
+async function isSolicitante(req, res, next) {
+  try {
+    console.log("Pase por la autorización");
+    const user = await User.findOne({ email: req.email });
+    const roles = await Role.find({ _id: { $in: user.roles } });
+
+    for (const role of roles) {
+      if (role.name === "solicitante") {
+        next();
+        return;
+      }
+    }
+
+    return respondError(
+      req,
+      res,
+      401,
+      "Se requiere un rol de solicitante para realizar esta acción"
+    );
+  } catch (error) {
+    handleError(error, "authorization.middleware -> isSolicitante");
+  }
+}
+
+/**
+ * Comprueba si el usuario es encargado
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ * @param {Function} next - Función para continuar con la siguiente función
+ */
+async function isEncargadoVis(req, res, next) {
+  try {
+    console.log("Pase por la autorización");
+    const user = await User.findOne({ email: req.email });
+    const roles = await Role.find({ _id: { $in: user.roles } });
+
+    for (const role of roles) {
+      if (role.name === "encargado") {
+        next();
+        return;
+      }
+    }
+
+    return respondError(
+      req,
+      res,
+      401,
+      "Se requiere un rol de encargado de visitas para realizar esta acción"
+    );
+  } catch (error) {
+    handleError(error, "authorization.middleware -> isEncargadoVis");
+  }
+}
+
 module.exports = {
   isAdmin,
+  isSolicitante,
+  isEncargadoVis
 };
