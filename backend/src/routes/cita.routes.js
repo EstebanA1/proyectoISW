d858e3b76e51ab8eef20b82e8d0ab0678cf472b9
EstebanA1 <esteban.rivas2001@alumnos.ubiobio.cl>
2023-10-17 19:57:54 -0300

    Se agregaron las validaciones para el administrador

--- backend/src/routes/cita.routes.js
+++ backend/src/routes/cita.routes.js
@@ -2,15 +2,27 @@
 
 const express = require("express");
 
+/** Middlewares de autorización */
+const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
+
+/** Middlewares de autenticacion */
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
+
+/** Controller de cita*/
 const citaController = require("../controllers/cita.controller");
 
+/** Instancia del enrutador */
 const router = express.Router();
 
-router.get('/', citaController.getCitas);
-router.post('/', citaController.createCita);
-router.get('/:id', citaController.getCitaById);
-router.put('/:id', citaController.updateCita);
-router.delete('/:id', citaController.deleteCita);
+// Define el middleware de autenticación para todas las rutas
+router.use(authenticationMiddleware);
+
+// Define las rutas para las citas
+router.get('/',  authorizationMiddleware.isAdmin, citaController.getCitas);
+router.post('/', authorizationMiddleware.isAdmin, citaController.createCita);
+router.get('/:id', authorizationMiddleware.isAdmin, citaController.getCitaById);
+router.put('/:id', authorizationMiddleware.isAdmin, citaController.updateCita);
+router.delete('/:id', authorizationMiddleware.isAdmin, citaController.deleteCita);
 
 
 module.exports = router;
