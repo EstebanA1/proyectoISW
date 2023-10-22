e56f1a3aa420268c0452a67208ad237db4d9d378
bdreyes12 <brandon.reyes2001@alumnos.ubiobio.cl>
2023-10-22 00:10:32 -0300

    Se crea modelo de solicitud

---
+++ backend/src/models/solicitud.model.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Importa el modulo 'mongoose' para crear la conexion a la base de datos
+const mongoose = require("mongoose");
+
+// Crea el esquema de la coleccion 'solicitudes'
+
+const solicitudSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLenght: 2
+        },
+        typeOfRequest: {
+            type: String,
+            required: true,
+            minLenght: 5
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        versionKey: false,
+    },
+);
+
+/** Modelo de datos 'Solicitud' */
+const Solicitud = mongoose.model("Solicitud", solicitudSchema);
+
+// Exporta el modelo de datos 'Solicitud'
+module.exports = Solicitud;
+
