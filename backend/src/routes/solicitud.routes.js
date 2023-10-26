94fb1f472f1d7f48bf923aa5e9ad912881684822
CarlxsSc <carlos.sanchez2002@alumnos.ubiobio.cl>
2023-10-26 00:14:30 -0300

    Modificacion ruta de solicitud

--- backend/src/routes/solicitud.routes.js
+++ backend/src/routes/solicitud.routes.js
@@ -2,20 +2,29 @@
 
 const express = require("express");
 
+/** Middlewares de autorizacion */
 const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
 
+/** Middlewares de autenticacion */
 const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 
+/** Controller de solicitud*/
 const solicitudController = require("../controllers/solicitud.controller");
 
+/** Instancia del enrutador */
 const router = express.Router();
 
+// Define el middleware de autenticacion para todas las rutas
 router.use(authenticationMiddleware);
 
+// Define las rutas para las solicitudes
 router.get('/', solicitudController.getSolicitudes);
 router.post('/', solicitudController.createSolicitud);
 router.get('/:id', solicitudController.getSolicitudById);
 router.put('/:id',  solicitudController.updateSolicitud);
 router.delete('/:id', solicitudController.deleteSolicitud);
+
+
+// Hacer quien ve las solicitudes.
 
 module.exports = router;
