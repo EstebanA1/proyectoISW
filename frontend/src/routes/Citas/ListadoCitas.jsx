8b821b83594fc904a72063204898b02265dc430c
EstebanA1 <esteban.rivas2001@alumnos.ubiobio.cl>
2023-12-16 09:18:31 -0300

    A

--- frontend/src/routes/Citas/ListadoCitas.jsx
+++ frontend/src/routes/Citas/ListadoCitas.jsx
@@ -1,17 +1,23 @@
 import { Grid } from '@mui/material'
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import EditIcon from '@mui/icons-material/Edit';
 import React, { useEffect, useState } from 'react';
+import { getCitas } from '../../services/cita.service';
+import { Box } from '@mui/system';
+
+import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/LibraryAdd';
 import InfoIcon from '@mui/icons-material/Visibility';
-import { getCitas } from '../../services/cita.service';
+
+import { handleDelete } from './DetailsCita';
+import { useSnackbar } from 'notistack';
 
 const Citas = () => {
     const router = useNavigate();
     const [citas, setCitas] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
         getCitas().then((res) => {
@@ -33,6 +39,7 @@ const Citas = () => {
                 height: '75vh'
             }}>
                 <h1>Listado de Citas</h1>
+                <div className='line' style={{ width: '85%' }}></div>
                 <Grid sx={{
                     display: 'flex',
                     alignItems: 'right',
@@ -54,20 +61,31 @@ const Citas = () => {
                     />
                     <Button type="button" variant="contained" sx={{ mr: 2, ml: 2 }} onClick={() => router(`/citas/create/`)}><AddIcon /></Button>
                 </Grid>
-                {citas.filter((cita) => cita.name.toLowerCase().includes(searchTerm.toLowerCase())).map((cita, index) => (
-                    <div key={cita._id}>
-                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                            <span>{index + 1}.</span>
-                            <span>{cita.name}</span>
-                        </div>
-                        <div style={{ display: 'flex', justifyContent: 'end' }}>
-                            <h5>{cita.date}</h5>
-                        </div>
-                        <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/citas/${cita._id}`)}><InfoIcon /></Button>
-                        <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/citas/update/${cita._id}`)}><EditIcon /></Button>
-                        <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/citas/delete/${cita._id}`)}><DeleteIcon /></Button>
-                    </div>
-                ))}
+
+                <Box sx={{ overflowY: 'auto' }}>
+                    <Box sx={{ mr: 2 }}>
+                    <div class="lineaNegra"></div>
+                        {citas.filter((cita) => cita.name.toLowerCase().includes(searchTerm.toLowerCase())).map((cita, index) => (
+                            <div key={cita._id}>
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <span>{index + 1}.</span>
+                                    <span>{cita.name}</span>
+                                </div>
+                                <div style={{ display: 'flex', justifyContent: 'end' }}>
+                                    <h5>{cita.date}</h5>
+
+                                </div>
+                                <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/citas/${cita._id}`)}><InfoIcon /></Button>
+                                <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/citas/update/${cita._id}`)}><EditIcon /></Button>
+                                <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => handleDelete(cita._id, enqueueSnackbar, router)}><DeleteIcon /></Button>
+                                <Button sx={{ ml: 1, mb: 3, mt: -1 }} type="button" variant="contained" onClick={() => router(`/feedback/create/${cita._id}`)}>Crear Retroalimentacion</Button>
+                                <div class="lineaNegra"></div>
+
+                            </div>
+
+                        ))}
+                    </Box>
+                </Box>
                 <br />
             </Grid>
         </>
